26ae8d2c247b3a5a81f84e2e404e1731db6d44a2
Rakshit Dhawan <rdhawan@cvent.com>
2020-07-11 04:21:52 +0530

    Reading request Body

--- app.js
+++ app.js
@@ -15,9 +15,25 @@ const server = http.createServer((req,res)=> {
     }
    //  process.exit(); -- hard exits the event loop (do not use in your code)
     if(url=='/message' && method==='POST'){
+
+        const body= [];
+
+        req.on('data' , (chunk)=> {
+             console.log(chunk);
+            body.push(chunk);
+        })
+
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+        })
+
         fs.writeFileSync('message.txt', 'Dummy');
+
         res.statusCode=302;
+
         res.setHeader('Location','/');
+
         return res.end();
     }
    res.setHeader('Content-Type','text/html');
