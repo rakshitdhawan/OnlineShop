ea52f95316d6d3ca676eccaaf5f4be903742b235
Rakshit Dhawan <rdhawan@cvent.com>
2020-07-11 04:09:11 +0530

    Redirection

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const http = require('http');
+const fs= require('fs');
 
 
 
 const server = http.createServer((req,res)=> {
-    console.log(req.url,req.method,req.headers);
+    const url = req.url;
+    const method = req.method;
+    if(url==='/'){
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>')
+        res.write('<body><form action= "/message" method = "POST"><input type = "text" name="message"><br><button type="submit">Send</button></form></body>')
+        res.write('</html>')
+        return res.end();
+    }
    //  process.exit(); -- hard exits the event loop (do not use in your code)
+    if(url=='/message' && method==='POST'){
+        fs.writeFileSync('message.txt', 'Dummy');
+        res.statusCode=302;
+        res.setHeader('Location','/');
+        return res.end();
+    }
    res.setHeader('Content-Type','text/html');
    res.write('<html>');
    res.write('<head><title>My First Page</title></head>')
