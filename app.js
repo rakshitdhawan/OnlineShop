087670041c95b2303a44ddf72719b12172d3658c
Rakshit Dhawan <rdhawan@cvent.com>
2020-07-10 19:40:17 +0530

    First Code

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+
+
+const server = http.createServer((req,res)=> {
+    console.log(req.url,req.method,req.headers);
+   //  process.exit(); -- hard exits the event loop (do not use in your code)
+   res.setHeader('Content-Type','text/html');
+   res.write('<html>');
+   res.write('<head><title>My First Page</title></head>')
+   res.write('<body><h1>Hello</h1></body>')
+   res.write('</html>')
+   res.end();
+});
+
+server.listen(3000);
