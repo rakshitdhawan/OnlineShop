1ee0206fd79ae54984cf1b32ee144070cfc72dae
Rakshit Dhawan <rdhawan@cvent.com>
2020-07-11 23:51:00 +0530

    Using Routes and Exporting methods

--- app.js
+++ app.js
@@ -1,45 +1,8 @@
 const http = require('http');
-const fs= require('fs');
+const routes = require('./routes');
 
 
-
-const server = http.createServer((req,res)=> {
-    const url = req.url;
-    const method = req.method;
-    if(url==='/'){
-        res.write('<html>');
-        res.write('<head><title>My First Page</title></head>')
-        res.write('<body><form action= "/message" method = "POST"><input type = "text" name="message"><br><button type="submit">Send</button></form></body>')
-        res.write('</html>')
-        return res.end();
-    }
-   //  process.exit(); -- hard exits the event loop (do not use in your code)
-    if(url=='/message' && method==='POST'){
-
-        const body= [];
-
-        req.on('data' , (chunk)=> {
-             console.log(chunk);
-            body.push(chunk);
-        })
-
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, err => {
-                res.statusCode=302;
-                res.setHeader('Location','/');
-                return res.end();
-    
-            });
-        });
-    }
-   res.setHeader('Content-Type','text/html');
-   res.write('<html>');
-   res.write('<head><title>My First Page</title></head>')
-   res.write('<body><h1>Hello</h1></body>')
-   res.write('</html>')
-    res.end();
-});
-
+const server = http.createServer(routes) 
+//const server = http.createServer(routes.handler); 
+//console.log(routes.someText);
 server.listen(3000);
