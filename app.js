6579d5196a720c89eede1bbb043250b8deb384f3
Rakshit Dhawan <rdhawan@cvent.com>
2020-07-12 18:36:04 +0530

    Using Middleware

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-const http = require('http');
-
-//const routes = require('./routes');
-
 const express = require('express');
 
 const app= express();
 
-const server = http.createServer(app) ;
-//const server = http.createServer(routes.handler); 
-//console.log(routes.someText);
-server.listen(3000);
+// app.use((req,res,next) => {
+//     console.log('In the middleware');
+//     next();                              //Allows the request to comtinue to next middleware in line
+// });
 
+
+app.use('/add-product',(req,res,next) => {
+    console.log('In another middleware');
+    res.send('<h1>Add Product Page</h1>');
+});
+
+app.use('/',(req,res,next) => {
+    console.log('In another middleware');
+    res.send('<h1>Hello from Express ! </h1>');
+});
+
+app.listen(3000);
