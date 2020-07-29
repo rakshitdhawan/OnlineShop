724b57c775dde9c21c95574ea583351a67dd9e96
Rakshit Dhawan <rdhawan@cvent.com>
2020-07-29 17:38:53 +0530

    Using Pug

--- app.js
+++ app.js
@@ -3,22 +3,30 @@ const bodyParser = require("body-parser");
 const app = express();
 const path = require("path");
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const rootDir = require("./util/path");
 // app.use((req,res,next) => {
 //     console.log('In the middleware');
 //     next();                              //Allows the request to comtinue to next middleware in line
 // });
+
+
+app.set('view engine', 'pug'); // Use PUG engine to combine dynamic template
+app.set('views','views'); //Where to find these templates , views is the directory name .
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
+
+
+
 app.use(express.static(path.join(__dirname,'public',)))
 //changing position of adminRoutes and shopRoutes will not change anything because we are using get method in these routes and get method checks for exact match unlike use method.
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+  res.status(404).render('404',{pageTitle:'Page Not Found'});
   console.log(rootDir);
 });
 
--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,14 @@
 const express = require('express');
 
 const path =require('path');
-
+const adminData=require('./admin');
 const rootDir = require ('../util/path');
 
 const router = express.Router();
 //get checks for exact match i.e '/' unlike use , which just matches the start
 router.get('/',(req,res,next) => {
-
-    res.sendFile(path.join(rootDir,'views','shop.html'));
+    const products = adminData.products;
+    res.render('shop',{prods : products, pageTitle:'Shop',path :'/shop'});
 });
 
 module.exports=router;
