75b287955e236244d339aa70cfdbb37ecf431b39
Rakshit Dhawan <rdhawan@cvent.com>
2020-07-31 00:15:05 +0530

    Implementing MVC

--- app.js
+++ app.js
@@ -3,32 +3,25 @@ const bodyParser = require("body-parser");
 const app = express();
 const path = require("path");
 const expressHbs=require('express-handlebars')
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const rootDir = require("./util/path");
-// app.use((req,res,next) => {
-//     console.log('In the middleware');
-//     next();                              //Allows the request to comtinue to next middleware in line
-// });
 
-// app.engine('hbs',expressHbs({layoutsDir:'views/layouts/',defaultLayout:'main-layout',extname:'hbs'})); Handlebar registration
+const pageNotFoundController = require('./controllers/error')
+
 app.set('view engine', 'ejs'); // Use PUG engine to combine dynamic template
 app.set('views','views'); //Where to find these templates , views is the directory name .
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes.routes);
 app.use(shopRoutes);
 
 
 
 app.use(express.static(path.join(__dirname,'public',)))
-//changing position of adminRoutes and shopRoutes will not change anything because we are using get method in these routes and get method checks for exact match unlike use method.
 
-app.use((req, res, next) => {
-  res.status(404).render('404',{pageTitle:'Page Not Found'});
-  console.log(rootDir);
-});
+
+app.use(pageNotFoundController.pageNotFound);
 
 app.listen(3000);
 //app.get and app.post can be used for get and post requests
--- routes/shop.js
+++ routes/shop.js
@@ -2,18 +2,11 @@ const express = require('express');
 
 const path =require('path');
 const adminData=require('./admin');
-const rootDir = require ('../util/path');
+
+const productsController = require('../controllers/products')
 
 const router = express.Router();
-//get checks for exact match i.e '/' unlike use , which just matches the start
-router.get('/',(req,res,next) => {
-    const products = adminData.products;
-    res.render('shop',{prods : products,
-                         pageTitle:'Shop',
-                         path :'/shop',
-                         hasProducts:products.length>0,
-                          activeShop:true ,
-                          productCSS:true});
-});
+
+router.get('/',productsController.getProducts);
 
 module.exports=router;
